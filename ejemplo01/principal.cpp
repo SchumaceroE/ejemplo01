25632f4504c01bc09151be470919aa80d950c5f9
Chuma26 <simonchumacero26@gmail.com>
2021-09-09 17:16:13 -0400

    Add project files.

---
+++ ejemplo01/principal.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Mi nombre es : Chumacero Espada Simon" << endl<<endl;
+	cout << "Mi C.U. es : 111-289" << endl<<endl;
+	cout << "Carrera: Ing. En Ciencias de la Computacion" << endl<<endl;
+
+	return 0;
+
+}
+
